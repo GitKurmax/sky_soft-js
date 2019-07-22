930101d3bbac5cbe595ea64d8a98d96374e3b71f
Alex <alexandrmail32@gmail.com>
2019-07-22 12:25:04 +0300

    Right solution for singleton

--- singletone.js
+++ singletone.js
@@ -1,9 +1,13 @@
 function MySingleton () {
+  MySingleton.count;
+  if(!MySingleton.count) {
+    MySingleton.count = 1;
+    MySingleton.instance = new MySingleton;
+  }
   if (MySingleton.instance) {
 		return MySingleton.instance
   }
-  MySingleton.instance = this;
-  return MySingleton.instance;
+  
 }
 
 MySingleton.prototype.download = function(url){ };
@@ -12,6 +16,8 @@ var a = MySingleton();
 var b = new MySingleton();
 var c = new MySingleton();
 var d = MySingleton();
+
+console.log(a);
 
 console.log(a===b); // true
 console.log(b===c); // true
